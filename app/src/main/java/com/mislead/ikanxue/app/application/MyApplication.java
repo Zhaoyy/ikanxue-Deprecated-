ff53fac93f031744012f1b6a90b1c24e75301448
zhaoyangyang <867986737@qq.com>
2015-06-29 15:53:27 +0800

    初步完成闪屏

---
+++ app/src/main/java/com/mislead/ikanxue/app/application/MyApplication.java
@@ -0,0 +1,21 @@
+package com.mislead.ikanxue.app.application;
+
+import android.app.Application;
+import com.mislead.ikanxue.app.util.ShPreUtil;
+import com.mislead.ikanxue.app.util.VolleyHelper;
+
+/**
+ * MyApplication
+ * AUTHOR:Zhaoyy  2015/6/29
+ * DESC:
+ **/
+public class MyApplication extends Application {
+
+  private static String TAG = "MyApplication";
+
+  @Override public void onCreate() {
+    super.onCreate();
+    ShPreUtil.init(getApplicationContext());
+    VolleyHelper.init(getApplicationContext());
+  }
+}
