78183a90654b040fa833f8adaaf3f8fca675548c
zhaoyangyang <867986737@qq.com>
2015-09-16 17:48:46 +0800

    使用增强的loghelper

--- app/src/main/java/com/mislead/ikanxue/app/util/LogHelper.java
+++ app/src/main/java/com/mislead/ikanxue/app/util/LogHelper.java
@@ -1,6 +1,8 @@
 package com.mislead.ikanxue.app.util;
 
+import android.text.TextUtils;
 import android.util.Log;
+import com.mislead.ikanxue.app.BuildConfig;
 
 /**
  * LogHelper
@@ -11,21 +13,81 @@ public class LogHelper {
 
   private static String TAG = "Mislead";
 
-  public static boolean isDebug = true;
+  static String tagPrefix = "cp:";
+  static String className;
+  static String methodName;
+  static int lineNumber;
 
-  public static void e(String msg) {
-    e(TAG, msg);
+  private LogHelper() {
+        /* Protect from instantiations */
   }
 
-  public static void e(String tag, String msg) {
-    if (isDebug) Log.e(tag, msg);
+  public static boolean isDebuggable() {
+    return BuildConfig.DEBUG;
   }
 
-  public static void i(String tag, String msg) {
-    if (isDebug) Log.i(tag, msg);
+  private static String createLog(String log) {
+
+    StringBuffer buffer = new StringBuffer();
+    buffer.append("[");
+    buffer.append(methodName);
+    buffer.append(":");
+    buffer.append(lineNumber);
+    buffer.append("]");
+    buffer.append(log);
+
+    return buffer.toString();
   }
 
-  public static void i(String msg) {
-    i(TAG, msg);
+  private static void getMethodNames(StackTraceElement[] sElements) {
+    className = sElements[1].getFileName();
+    if (!TextUtils.isEmpty(tagPrefix)) {
+      className = tagPrefix + className;
+    }
+    methodName = sElements[1].getMethodName();
+    lineNumber = sElements[1].getLineNumber();
+  }
+
+  public static void e(String message) {
+    if (!isDebuggable()) return;
+
+    // Throwable instance must be created before any methods
+    getMethodNames(new Throwable().getStackTrace());
+    Log.e(className, createLog(message));
+  }
+
+  public static void i(String message) {
+    if (!isDebuggable()) return;
+
+    getMethodNames(new Throwable().getStackTrace());
+    Log.i(className, createLog(message));
+  }
+
+  public static void d(String message) {
+    if (!isDebuggable()) return;
+
+    getMethodNames(new Throwable().getStackTrace());
+    Log.d(className, createLog(message));
+  }
+
+  public static void v(String message) {
+    if (!isDebuggable()) return;
+
+    getMethodNames(new Throwable().getStackTrace());
+    Log.v(className, createLog(message));
+  }
+
+  public static void w(String message) {
+    if (!isDebuggable()) return;
+
+    getMethodNames(new Throwable().getStackTrace());
+    Log.w(className, createLog(message));
+  }
+
+  public static void wtf(String message) {
+    if (!isDebuggable()) return;
+
+    getMethodNames(new Throwable().getStackTrace());
+    Log.wtf(className, createLog(message));
   }
 }
